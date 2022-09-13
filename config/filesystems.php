ff95eaf358d1d64f22830a9663d6bfa7b049bb67
Esterling Accime Youtube <accimeesterlin@yahoo.com>
2022-09-12 23:53:05 -0700

    adding s3 file configuration

--- config/filesystems.php
+++ config/filesystems.php
@@ -123,6 +123,11 @@ return [
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
+            'cach' => [
+                'store' => 'memcached',
+                'expire' => 600,
+                'prefix' => 'cache-prefix'
+            ]
         ],
 
         'minio' => [
